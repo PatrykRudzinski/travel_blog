145d77e7f729d6019c3514e753e1ec815216706c
Patryk Rudziński <patrykrudzinski@gmail.com>
2019-04-18 22:45:33 +0200

    TypeScript!

--- components/Pages/About/About.tsx
+++ components/Pages/About/About.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Layout } from '@components/Layout';
+import * as React from 'react';
+import { Layout } from './../../Layout';
 
 const About = () => (
   <Layout>
